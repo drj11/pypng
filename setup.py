78bdd80ee95cab2eed0b72fcfcce71daf621a81e
drj@pobox.com <drj@pobox.com@40d66c6c-0ee5-11de-8837-45bce846067a>
2009-03-16 21:13:03 +0000

    Getting some project metadata from setup.py
    git-svn-id: https://pypng.googlecode.com/svn/trunk@16 40d66c6c-0ee5-11de-8837-45bce846067a

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 # $URL$
 # $Rev$
 
-from distutils.core import setup
-setup(name='pypng',
-      version='0.0.1',
-      description='Pure Python PNG encoder/decoder',
-      author='David Jones',
-      author_email='drj@pobox.com',
-      url='http://code.google.com/p/pypng/',
-      package_dir={'':'code'},
-      py_modules=['png'],
-      )
+conf = dict(
+    name='pypng',
+    version='0.0.1',
+    description='Pure Python PNG encoder/decoder',
+    author='David Jones',
+    author_email='drj@pobox.com',
+    url='http://code.google.com/p/pypng/',
+    package_dir={'':'code'},
+    py_modules=['png'],
+    )
 
+if __name__ == '__main__':
+    from distutils.core import setup
+    setup(**conf)
