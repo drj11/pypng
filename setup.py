cef9f19a6623b8b4ec63670a9032e08ddb12ff0c
drj@pobox.com <drj@pobox.com@40d66c6c-0ee5-11de-8837-45bce846067a>
2009-03-17 09:23:13 +0000

    Adding documentation to setup.py
    git-svn-id: https://pypng.googlecode.com/svn/trunk@20 40d66c6c-0ee5-11de-8837-45bce846067a

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 # $URL$
 # $Rev$
 
+# PyPNG setup.py
+# This is the setup.py script used by disutils.
+
+# You can install the png module into your Python distribution with:
+# python setup.py install
+# You can also do other standard disutil type things, but you can refer
+# to the disutil documentation for that.
+
+# This script is also imported as a module by the Sphinx conf.py script
+# in the man directory, so that they can share metadata.
+
 conf = dict(
     name='pypng',
     version='0.0.1',
