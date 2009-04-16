02c2b391d57a9650042b98f521a20ecf9f4cd10b
drj@pobox.com <drj@pobox.com@40d66c6c-0ee5-11de-8837-45bce846067a>
2009-04-16 20:35:16 +0000

    Updated example NumPy code.
    git-svn-id: https://pypng.googlecode.com/svn/trunk@126 40d66c6c-0ee5-11de-8837-45bce846067a

--- code/exnumpy.py
+++ code/exnumpy.py
@@ -6,15 +6,18 @@
 # Original code created by Mel Raab, modified by David Jones.
 
 '''
-  RGB PNG FILES, pypng and numpy
-  (abstracted from functioning code)
+  Example code integrating RGB PNG files, PyPNG and NumPy
+  (abstracted from Mel Raab's functioning code)
 '''
 
-import png
+# http://www.python.org/doc/2.4.4/lib/module-itertools.html
+import itertools
+
 import numpy
+import png
 
 
-''' If you have a png file for an RGB image,
+''' If you have a PNG file for an RGB image,
     and want to create a numpy array of data from it.
 '''
 # Read the file "picture.png" from the current directory.  The `Reader`
@@ -23,12 +26,11 @@ import numpy
 # an image from the internet:
 # png.Reader(file=urllib.urlopen('http://www.libpng.org/pub/png/PngSuite/basn2c16.png'))
 pngReader=png.Reader(filename='picture.png')
-pngAsDirect=pngReader.asDirect()
 # Tuple unpacking, using multiple assignment, is very useful for the
 # result of asDirect (and other methods).
 # See
 # http://docs.python.org/tutorial/introduction.html#first-steps-towards-programming
-row_count, column_count, pngdata, meta = pngAsDirect
+row_count, column_count, pngdata, meta = pngReader.asDirect()
 bitdepth=meta['bitdepth']
 plane_count=meta['planes']
 
@@ -40,33 +42,34 @@ assert plane_count == 3
            [R,G,B, R,G,B, R,G,B])
     Array dimensions for this example:  (2,9)
 
-    Create `image_boxed_row_flat_pixels` as a two-dimensional numpy
-    array of the right shape, then populate it row-by-row from PyPNG's data.
-    The numpy array mimics PyPNG's representation; it will have
-    dimensions ``(row_count,column_count*plane_count)``.
+    Create `image_2d` as a two-dimensional NumPy array by stacking a
+    sequence of 1-dimensional arrays (rows).
+    The NumPy array mimics PyPNG's (boxed row flat pixel) representation;
+    it will have dimensions ``(row_count,column_count*plane_count)``.
 '''
-image_boxed_row_flat_pixels=numpy.zeros((row_count,plane_count*column_count),
-                                      dtype=numpy.uint16)
-for row_index, one_boxed_row_flat_pixels in enumerate(pngdata):
-    image_boxed_row_flat_pixels[row_index,:]=one_boxed_row_flat_pixels
-# An alternative to the above is to use ``numpy.vstack``.  This is very
-# simple and direct:
-#   numpy.vstack(pngdata)
-# When used like this, numpy will pick the resulting array's data type;
-# in practice it seems to pick ``numpy.int32``, which is large enough to
-# hold any pixel value for any PNG image but uses 4 bytes per value when
-# 1 or 2 would be enough.  If you want to choose the result array's data
-# type, you can convert each row to a numpy array of the chosen type.
-# Here we use the fact that any numpy basic type can be used as a function
-# to convert an ordinary Python sequence to a numpy array.  See
-# http://docs.scipy.org/doc/numpy/user/basics.types.html for more
-# details.
-#   numpy.vstack(itertools.imap(numpy.uint16, pngdata))
+# The use of ``numpy.uint16``, below, is to convert each row to a NumPy
+# array with data type ``numpy.uint16``.  This is a feature of NumPy,
+# discussed further in 
+# http://docs.scipy.org/doc/numpy/user/basics.types.html .
+# You can use avoid the explicit conversion with
+# ``numpy.vstack(pngdata)``, but then NumPy will pick the array's data
+# type; in practice it seems to pick ``numpy.int32``, which is large enough
+# to hold any pixel value for any PNG image but uses 4 bytes per value when
+# 1 or 2 would be enough.
+# --- extract 001 start
+image_2d = numpy.vstack(itertools.imap(numpy.uint16, pngdata))
+# --- extract 001 end
 # Do not be tempted to use ``numpy.asarray``; when passed an iterator
 # (`pngdata` is often an iterator) it will attempt to create a size 1
 # array with the iterator as its only element.
+# An alternative to the above is to create the target array of the right
+# shape, then populate it row by row:
+if 0:
+    image_2d = numpy.zeros((row_count,plane_count*column_count),
+                           dtype=numpy.uint16)
+    for row_index, one_boxed_row_flat_pixels in enumerate(pngdata):
+        image_2d[row_index,:]=one_boxed_row_flat_pixels
 
-del pngAsDirect
 del pngReader
 del pngdata
 
@@ -77,16 +80,18 @@ del pngdata
                  [ (R,G,B), (R,G,B), (R,G,B) ])
     Array dimensions for this example:  (2,3,3)
 
-    ``data`` will contain the image as a three-dimensional numpy array
-         and have dimensions ``(row_count,column_count,plane_count)``.
+    ``image_3d`` will contain the image as a three-dimensional numpy
+    array, having dimensions ``(row_count,column_count,plane_count)``.
 '''
-data = numpy.reshape(image_boxed_row_flat_pixels,
-                     (row_count,column_count,plane_count))
+# --- extract 002 start
+image_3d = numpy.reshape(image_2d,
+                         (row_count,column_count,plane_count))
+# --- extract 002 end
 
 
 ''' ============= '''
 
-''' Convert numpy data array to PNG image file.
+''' Convert NumPy image_3d array to PNG image file.
 
     If the data is three-dimensional, as it is above, the best thing
     to do is reshape it into a two-dimensional array with a shape of
@@ -95,7 +100,7 @@ data = numpy.reshape(image_boxed_row_flat_pixels,
     directly to the ``png.Writer.write`` method.
 '''
 
-row_count, column_count, plane_count = data.shape
+row_count, column_count, plane_count = image_3d.shape
 assert plane_count==3
 
 pngfile = open('picture_out.png', 'wb')
@@ -109,13 +114,15 @@ try:
                            alpha=False,
                            bitdepth=16)
     # As of 2009-04-13 passing a numpy array that has an element type
-    # that is a numpy integer type (for example, the `data` array has an
+    # that is a numpy integer type (for example, the `image_3d` array has an
     # element type of ``numpy.uint16``) generates a deprecation warning.
     # This is probably a bug in numpy; it may go away in the future.
     # The code still works despite the warning.
     # See http://code.google.com/p/pypng/issues/detail?id=44
+# --- extract 003 start
     pngWriter.write(pngfile,
-                    numpy.reshape(data, (-1, column_count*plane_count)))
+                    numpy.reshape(image_3d, (-1, column_count*plane_count)))
+# --- extract 003 end
 finally:
     pngfile.close()
 
