fa0c58f7e2cdd8cb47d96bc987332251f8eb3360
drj11x@gmail.com <drj11x@gmail.com@40d66c6c-0ee5-11de-8837-45bce846067a>
2012-04-20 10:08:07 +0000

    typo
    git-svn-id: https://pypng.googlecode.com/svn/trunk@233 40d66c6c-0ee5-11de-8837-45bce846067a

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@
 
 # You can install the png module into your Python distribution with:
 # python setup.py install
-# You can also do other standard disutil type things, but you can refer
-# to the disutil documentation for that.
+# You can also do other standard distutil type things, but you can refer
+# to the distutil documentation for that.
 
 # This script is also imported as a module by the Sphinx conf.py script
 # in the man directory, so that this file forms a single source for
