06c394e0451a647708639eb1514bab73d7a764cc
David Jones <drj@pobox.com>
2015-08-10 10:27:14 +0100

    fix test_suite key in conf

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
         from setuptools import setup
         # distribute is probably installed, so use_2to3 should work
         conf['use_2to3'] = True
-        test_suite="test_png",
+        conf['test_suite'] = "test_png"
     except ImportError:
         # http://docs.python.org/release/2.4.4/dist/setup-script.html
         from distutils.core import setup
